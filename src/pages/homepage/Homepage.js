936173ae1a97518ef0907a6bfcfc7d80c45ba4c4
masonomara <78335051+masonomara@users.noreply.github.com>
2022-06-19 10:42:24 -0400

    sert up header formatting

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,13 +1,49 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
 // styles
 import './Homepage.css'
 
-// components
-import HomepageNavbar from '../../components/homepageNavbar/HomepageNavbar.js'
+// images
+import Logo from '../../assets/logo.png'
+import DownChevron from '../../assets/icons/20px/down.png'
 
 export default function Homepage() {
+
+  const [showHomeNavOptions, setShowHomeNavOptions] = useState(false)
+
+  const handleCloseHomeNavOptions = () => {
+    setShowHomeNavOptions(false)
+  }
+  const handleOpenHomeNavOptions = () => {
+    setShowHomeNavOptions(true)
+  }
+
   return (
       <div>
-        <HomepageNavbar />
+        <div className="header">
+          <div className="container--flex">
+            <div className="header__logo" onClick={handleOpenHomeNavOptions}>
+              <img className="header__logo--disco-ball" src={Logo} alt="disco ball" />
+              <span className="header__logo--text">Events</span>
+              <img className="header__logo--chevron" src={DownChevron} alt="chevron" />
+            </div>
+
+            {showHomeNavOptions &&
+            <div className="home__nav-options">
+              <div className="home__nav-options__events" onClick={handleCloseHomeNavOptions}>
+                Events
+              </div>
+              <NavLink className="home__nav-options__venues" to="/venues">
+                <div onClick={handleCloseHomeNavOptions}>
+                  Venues
+                </div>
+              </NavLink>  
+            </div>            
+            }
+
+          </div>  
+        </div>
       </div>
   )
 }
