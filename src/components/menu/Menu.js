74476526bd18c517f963f02dcd4b6cec5b922b3d
masonomara <78335051+masonomara@users.noreply.github.com>
2022-06-20 17:35:20 -0400

    set up linear gradient

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 // styles
 import './Menu.css'
 
@@ -8,19 +10,32 @@ export default function Menu({ handleCloseMenu }) {
   return (
       <div className="menu__backdrop">
         <div className="menu">
+
           <div className="menu__header">
-            <img className="icon--16px hidden" src={close} alt="close" />
-            <span className="font__header">Menu</span>
+            <div className="button__icon--low hidden">
+              <img className="icon--16px hidden" src={close} alt="close" />
+            </div>
+            <span className="font--header">Menu</span>
             <div className="button__icon--low" onClick={handleCloseMenu}>
-            <img className="icon--16px" src={close} alt="close" />
+              <img className="icon--16px" src={close} alt="close" />
             </div>
           </div>
+
           <div className="menu__cta">
+            <img />
+            <span className="font--title">Share your event through Asbury Beans</span>
+            <span id="lowdown" className="font--caption">Add your live event to our event calendar to promote your venue and Asbury Park</span>
+            <NavLink exact to="/submit" className="button--filled">
+              <span className="font--callout--low">Custom Package</span>
+            </NavLink>
           </div>
+
           <div className="menu__list">
           </div>
+
           <div className="menu__info">
           </div>
+
         </div>
       </div>
   )
