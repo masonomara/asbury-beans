4a2c8ea4556cbc88198e4bbdbf16b6e191ddb41e
masonomara <78335051+masonomara@users.noreply.github.com>
2022-06-21 01:04:25 -0400

    set up venue card

--- src/components/venuesGrid/VenuesGrid.js
+++ src/components/venuesGrid/VenuesGrid.js
@@ -1,22 +1,28 @@
 import { useCollection } from '../../hooks/useCollection.js'
 
-// components
-import Venue from '../venue/Venue.js'
-
 // styles
 import './VenuesGrid.css'
 
 export default function VenuesGrid() {
-const { isPending, error, documents } = useCollection('venues')
+  const { isPending, error, documents } = useCollection('venues')
 
   return (
     <div className="venues-grid">
       {isPending && <div>Loading venues...</div>}
       {error && <div className="error">{error}</div>}
       {documents && documents.map(venue => (
-        <div key={venue.id}>
-          <span>{venue.venueName}</span>
-          <Venue src={venue.photoURL} />
+        <div key={venue.id} className="venue-card">
+          <div className="venue-card__photo__container">
+            <img className="venuePhoto" src={venue.photoURL} alt=" " />
+          </div>
+          <div className="venue-card__info">
+            <span className="venue-card__title font--title">{venue.name}</span>
+            <div className="venue-card__address font--subtitle">
+              <span>{venue.address}</span>
+              <span>, </span>
+              <span>{venue.city}</span>
+            </div>
+          </div>
         </div>
       ))}
     </div>
