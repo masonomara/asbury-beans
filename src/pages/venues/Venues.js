d0eb04bb0edd0c3320fbb4c8c84bf9d1f0529b71
masonomara <78335051+masonomara@users.noreply.github.com>
2022-06-20 22:34:23 -0400

    fixed venue homepage

--- src/pages/venues/Venues.js
+++ src/pages/venues/Venues.js
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom'
 // styles
 import './Venues.css'
 
+// components
+import Menu from '../../components/menu/Menu.js'
+
 // images
 import logo from '../../assets/logo.png'
 import down from '../../assets/icons/down.png'
@@ -11,9 +14,9 @@ import check from '../../assets/icons/check.png'
 import menu from '../../assets/icons/menu.png'
 
 export default function Venues() {
-
   const [hideVenuesNavOptions, setHideVenuesNavOptions] = useState(true)
-
+  const [showMenu, setShowMenu] = useState(false)
+ 
   const handleCloseVenuesNavOptions = () => {
     setHideVenuesNavOptions(true)
   }
@@ -21,47 +24,54 @@ export default function Venues() {
     setHideVenuesNavOptions(false)
   }
 
+  const handleCloseMenu = () => {
+    setShowMenu(false)
+  }
+  const handleOpenMenu = () => {
+    setShowMenu(true)
+  }
+
   return (
-    <div>
-      <div className="header">
-        <div className="container--flex">
+      <div>
+        {showMenu && 
+        <Menu handleCloseMenu={handleCloseMenu} />
+        }
+        <div className="header">
+          <div className="container--flex">
 
-        {hideVenuesNavOptions ?
-          <div className="header__logo" onClick={handleOpenVenuesNavOptions}>
-            <img className="header__logo--disco-ball" src={logo} alt="disco ball" />
-            <span className="header__logo--text">Venues</span>
-            <img className="header__logo--chevron icon--16px" src={down} alt="chevron" />
-          </div> :
-
-          <div>
-          <div className="header__logo" onClick={handleCloseVenuesNavOptions}>
-            <img className="header__logo--disco-ball" src={logo} alt="disco ball" />
-            <span className="header__logo--text">Venues</span>
-            <img className="header__logo--chevron icon--16px" src={down} alt="chevron" />
-          </div>
-          <div className="header__nav-options">
-            <NavLink className="list-item header__nav-options__venues callout" to="/">
-              <div>
-                <span>Events</span>
+          {hideVenuesNavOptions ?
+            <div className="header__logo" onClick={handleOpenVenuesNavOptions}>
+              <img className="header__logo--disco-ball" src={logo} alt="disco ball" />
+              <span className="header__logo--text">Venues</span>
+              <img className="header__logo--chevron icon--16px" src={down} alt="chevron" />
+            </div> :
+            <div>
+            <div className="header__logo" onClick={handleCloseVenuesNavOptions}>
+              <img className="header__logo--disco-ball" src={logo} alt="disco ball" />
+              <span className="header__logo--text">Venues</span>
+              <img className="header__logo--chevron icon--16px" src={down} alt="chevron" />
+            </div>
+            <div className="header__nav-options">
+              <NavLink className="list-item font--callout" to="/">
+                  <span>Events</span>
+              </NavLink>  
+              <div className="break" />
+              <div className="list-item font--callout" onClick={handleCloseVenuesNavOptions}>
+                <span>Venues</span>
+                <img className="icon--16px header__nav-options__check" alt="check" src={check} />
               </div>
-            </NavLink>  
-            <div className="break" />
-            <div className="list-item header__nav-options__events callout" onClick={handleCloseVenuesNavOptions}>
-              <span>Venues</span>
-              <img className="icon--16px header__nav-options__check" alt="check" src={check} />
-            </div>
-          </div>   
-          </div>    
-          }
+            </div>   
+            </div>    
+            }
 
-          <div>
-            <div className="button__icon">
-              <img className="icon--24px" alt="menu" src={menu} />
-            </div>
-          </div>
 
-        </div>  
+            <div>
+              <div className="button__icon" onClick={handleOpenMenu}>
+                <img className="icon--24px" alt="menu" src={menu} />
+              </div>
+            </div>
+          </div>  
+        </div>
       </div>
-    </div>
   )
 }
